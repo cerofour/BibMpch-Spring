1c94fdf101989ca0440f969bd7825b3f39b5b980
Diego Llacsahuanga <llacsahuanga.buques@gmail.com>
2024-10-20 18:06:45 -0500

    Allow OPTIONS request from any origin
    - Change database name to bd_biblioteca

--- src/main/java/pe/edu/utp/BibMpch/configuration/SecurityConfiguration.java
+++ src/main/java/pe/edu/utp/BibMpch/configuration/SecurityConfiguration.java
@@ -3,19 +3,14 @@ package pe.edu.utp.BibMpch.configuration;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -29,6 +24,7 @@ public class SecurityConfiguration {
 		http
 				.csrf(AbstractHttpConfigurer::disable)
 				.authorizeHttpRequests((authRequest) -> authRequest
+						.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()  // Allow OPTIONS requests
 						.requestMatchers("/auth/*").permitAll()
 						.anyRequest().authenticated())
 				.sessionManagement(session -> session
