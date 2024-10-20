f796aff1a43922e8cbf0e06b0e0276bea4dd7cff
Diego Llacsahuanga <llacsahuanga.buques@gmail.com>
2024-10-20 14:16:38 -0500

    FIX: Use RepositoryRestConfigurer for CORS configuration
    - Change database name to bd_biblioteca

--- src/main/java/pe/edu/utp/BibMpch/configuration/CorsConfiguration.java
+++ src/main/java/pe/edu/utp/BibMpch/configuration/CorsConfiguration.java
@@ -6,14 +6,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration {
+public class CorsConfiguration implements WebMvcConfigurer {
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+						.allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+						.allowedOrigins("*");
 			}
 		};
 	}
