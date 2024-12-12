efaac2ad2e9c5d17f655571c1c9d6fcbe91b631c
Diego Llacsahuanga <llacsahuanga.buques@gmail.com>
2024-12-11 19:07:12 -0500

    Final version of the REST API

--- src/main/java/pe/edu/utp/BibMpch/controller/RoleController.java
+++ src/main/java/pe/edu/utp/BibMpch/controller/RoleController.java
@@ -1,11 +1,12 @@
 package pe.edu.utp.BibMpch.controller;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import pe.edu.utp.BibMpch.model.Roles;
-import pe.edu.utp.BibMpch.repository.RoleRepository;
+import pe.edu.utp.BibMpch.authorization.UserRole;
+import pe.edu.utp.BibMpch.authorization.UserRoleRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,22 +16,23 @@ import java.util.List;
 @AllArgsConstructor
 public class RoleController {
 
-    private final RoleRepository roleRepository;
+    private final UserRoleRepository userRoleRepository;
 
     @GetMapping(value = "/")
     @SuppressWarnings("unused")
-    public ResponseEntity<List<Roles>> getAllRoles() {
-        List<Roles> roles = new ArrayList<>();
-        roleRepository.findAll().forEach(roles::add);
+    public ResponseEntity<List<UserRole>> getAllRoles() {
+        List<UserRole> roles = new ArrayList<>();
+        userRoleRepository.findAll().forEach(roles::add);
         return ResponseEntity.ok(roles);
     }
 
     @GetMapping(value = "/get")
     @SuppressWarnings("unused")
-    public ResponseEntity<Roles> getRole(
-            @RequestParam(name = "id") Long id) {
+    public ResponseEntity<UserRole> getRole(
+            @RequestParam(name = "id") Short id) {
 
-        Roles role = roleRepository.findById(id).get();
+        UserRole role = userRoleRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Rol no encontrado."));
 
         return (role != null) ? ResponseEntity.ok(role)
                 : ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
